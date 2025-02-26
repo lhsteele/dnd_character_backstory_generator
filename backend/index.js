ef097c9885aa0b280d5b4b64371b418bac33ec40
Lisa Steele <lisa@walrus.ai>
2025-02-26 13:44:33 -1000

    Updates backend index.js with allowedOrigins instead of relying on env for CORS

--- backend/index.js
+++ backend/index.js
@@ -2,31 +2,33 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-const FRONTEND_URL = (process.env.NODE_ENV = "production"
-  ? "https://dnd-character-backstory-generator.vercel.app"
-  : "http://localhost:5173");
-
-// Initialize dotenv
 dotenv.config();
 
-// Initialize express
 const app = express();
 
-// Use CORS
-app.use(
-  cors({
-    origin: FRONTEND_URL,
-    methods: "GET, POST",
-    allowedHeaders: "Content-Type",
-  })
-);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://dnd-character-backstory-generator.vercel.app",
+];
 
-// Define routes
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET, POST",
+  allowedHeaders: "Content-Type",
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
